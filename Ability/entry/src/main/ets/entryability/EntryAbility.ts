a9d69cab602fc68c422cf90fa5d6ca322f9defe4
Melendez <markmelendez2008@outlook.com>
2022-12-05 22:55:52 +0800

    生命周期

--- Ability/entry/src/main/ets/entryability/EntryAbility.ts
+++ Ability/entry/src/main/ets/entryability/EntryAbility.ts
@@ -3,18 +3,25 @@ import Ability from '@ohos.application.Ability'
 import Window from '@ohos.window'
 
 export default class EntryAbility extends Ability {
+    /***
+     * onCreate 创建Ability时的回调
+     */
     onCreate(want, launchParam) {
         hilog.isLoggable(0x0000, 'testTag', hilog.LogLevel.INFO);
         hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
         hilog.info(0x0000, 'testTag', '%{public}s', 'want param:' + JSON.stringify(want) ?? '');
         hilog.info(0x0000, 'testTag', '%{public}s', 'launchParam:' + JSON.stringify(launchParam) ?? '');
     }
-
+    /***
+     * onDestory 销毁Ability时的回调
+     */
     onDestroy() {
         hilog.isLoggable(0x0000, 'testTag', hilog.LogLevel.INFO);
         hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onDestroy');
     }
-
+    /***
+     * onWindowStageCreate 窗口（小窗）创建时的回调
+     */
     onWindowStageCreate(windowStage: Window.WindowStage) {
         // Main window is created, set main page for this ability
         hilog.isLoggable(0x0000, 'testTag', hilog.LogLevel.INFO);
@@ -30,19 +37,25 @@ export default class EntryAbility extends Ability {
             hilog.info(0x0000, 'testTag', 'Succeeded in loading the content. Data: %{public}s', JSON.stringify(data) ?? '');
         });
     }
-
+    /***
+     * onWindowStageCreate 窗口（小窗）销毁时的回调
+     */
     onWindowStageDestroy() {
         // Main window is destroyed, release UI related resources
         hilog.isLoggable(0x0000, 'testTag', hilog.LogLevel.INFO);
         hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageDestroy');
     }
-
+    /***
+     * 切至前台时的回调
+     */
     onForeground() {
         // Ability has brought to foreground
         hilog.isLoggable(0x0000, 'testTag', hilog.LogLevel.INFO);
         hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onForeground');
     }
-
+    /***
+     * 切至后台时的回调
+     */
     onBackground() {
         // Ability has back to background
         hilog.isLoggable(0x0000, 'testTag', hilog.LogLevel.INFO);
